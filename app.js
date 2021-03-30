22b7741765548e37dd5a65978cdb18f838630f05
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-03-30 00:44:22 -0500

    LAB10_MARUTHI

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var compartmentRouter = require('./routes/compartment');
-
+var starsRouter = require('./routes/stars');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/compartment', compartmentRouter);
+app.use('/stars', starsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
